1a65a0f63463e03da918e5b536e28d8095541274
carlos henrique <carlos.contato34@gmail.com>
2023-04-30 18:58:32 -0300

    Implementing possible moves of Rook

--- src/boardgame/Position.java
+++ src/boardgame/Position.java
@@ -1,31 +1,40 @@
 package boardgame;
 
 public class Position {
-	
-	
+
 	private int Row;
 	private int column;
+
 	public Position(int pow, int column) {
 		this.Row = pow;
 		this.column = column;
 	}
+
 	public int getRow() {
 		return Row;
 	}
+
 	public void setRow(int pow) {
 		this.Row = pow;
 	}
+
 	public int getColumn() {
 		return column;
 	}
+
 	public void setColumn(int column) {
 		this.column = column;
 	}
-	
+
+	public void setValues(int row, int column) {
+		this.Row = row;
+		this.column = column;
+	}
+
 	@Override
-	
+
 	public String toString() {
-		return Row + ", "+ column;
+		return Row + ", " + column;
 	}
 
 }
